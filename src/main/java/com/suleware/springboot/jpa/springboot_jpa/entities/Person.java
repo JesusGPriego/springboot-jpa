b65b2a504855d61e4b0937ad62dbe148332d29c8
JesusGPriego <suleware@gmail.com>
2024-10-12 16:35:48 +0200

    configs set

--- src/main/java/com/suleware/springboot/jpa/springboot_jpa/entities/Person.java
+++ src/main/java/com/suleware/springboot/jpa/springboot_jpa/entities/Person.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +13,7 @@ import lombok.Setter;
 import lombok.ToString;
 
 @Entity
+@Table(name = "persons")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Jesús', 'García', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Pepe', 'Doe', 'Python');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('John', 'Dow', 'C++');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Maria', 'Roe', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Josefa', 'Rae', 'Java');
+INSERT INTO persons (name, lastname, programming_language) VALUES ('Josefa', 'Puebla', 'Javascript');
